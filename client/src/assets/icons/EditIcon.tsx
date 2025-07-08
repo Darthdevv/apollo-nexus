a21ec6774c77091f3436bf8e8e9c66a58c962d56
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2025-07-08 11:57:00 +0300

    ⚛️  feat: adding useMobile custom hook

--- client/src/assets/icons/EditIcon.tsx
+++ client/src/assets/icons/EditIcon.tsx
@@ -1,8 +1,11 @@
+import { useIsMobile } from "../../hooks/use-mobile";
 const EditIcon = () => {
+  const isMobile = useIsMobile();
+
   return (
     <svg
-      width={24}
-      height={24}
+      width={isMobile ? 20 : 24}
+      height={isMobile ? 20 : 24}
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
